6b99c31ad1f6b0a684dcbb7a876b7a811610402f
Trung047 <trunggn123@gmail.com>
2026-06-08 15:56:43 +0700

    DatabaseConnection.java

---
+++ Database/DatabaseConnection.java
@@ -0,0 +1,8 @@
+package database;
+
+public class DatabaseConnection {
+    // Viết code kết nối CSDL ở đây
+    public void connect() {
+        System.out.println("Đang kết nối tới Database...");
+    }
+}
