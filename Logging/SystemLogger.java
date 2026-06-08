eaea7aded867af19b038437ff6a59b46ae706cec
Trung047 <trunggn123@gmail.com>
2026-06-08 16:02:45 +0700

    Update SystemLogger.java

--- Logging/SystemLogger.java
+++ Logging/SystemLogger.java
@@ -2,12 +2,9 @@ package utils;
 
 public class SystemLogger {
     public static void logInfo(String message) {
-        // Code ghi lại các sự kiện bình thường của hệ thống
         System.out.println("[INFO] " + message);
     }
-
     public static void logError(String error) {
-        // Code ghi lại lỗi hệ thống
         System.err.println("[ERROR] " + error);
     }
 }
