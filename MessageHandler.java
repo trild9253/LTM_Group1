c4e7e6c20b87fcb4b59c25c2b8ea1bf3ea5c0728
trild9253 <trild9253@ut.edu.vn>
2026-06-21 19:36:59 +0700

    thay đổi messageHandler
    Thêm hàm handleFromServer để làm trạm trung chuyển dữ liệu giữa mạng kết nối và giao diện hiển thị.

--- MessageHandler.java
+++ MessageHandler.java
@@ -1,26 +1,28 @@
 // Xử lý tin nhắn
-public class MessageHandler{
+public class MessageHandler {
 
-    // Hàm xử lý tin nhắn nhận được
-    public static String handleMessage(String sender,String message){
-
+    public static String handleMessage(String sender, String message) {
         // Nếu là chat riêng (@Tên nội_dung)
-        if(message.startsWith("@")){
-            String[] data=message.split(" ",2);
+        if (message.startsWith("@")) {
+            String[] data = message.split(" ", 2);
 
-            if(data.length>1){
-                String receiver=data[0].substring(1);
-                String content=data[1];
+            if (data.length > 1) {
+                String receiver = data[0].substring(1);
+                String content = data[1];
 
-                Message msg=new Message(sender,receiver,content,Protocol.PRIVATE);
+                Message msg = new Message(sender, receiver, content, Protocol.PRIVATE);
 
-                return "[Private] "+msg.getSender()+" -> "+msg.getReceiver()+" : "+msg.getContent();
+                return "[Private] " + msg.getSender() + " -> " + msg.getReceiver() + " : " + msg.getContent();
             }
         }
 
         // Nếu không có @ thì chat nhóm
-        Message msg=new Message(sender,null,message,Protocol.GROUP);
+        Message msg = new Message(sender, null, message, Protocol.GROUP);
+        return "[" + msg.getSender() + "] : " + msg.getContent();
+    }
 
-        return "["+msg.getSender()+"] : "+msg.getContent();
+    //Hàm để hứng tin nhắn từ Client.java bắn qua
+    public static void handleFromServer(String rawMessage) {
+        System.out.println("[SERVER TRẢ VỀ THÔ]: " + rawMessage);
     }
 }
